3e9dfaddac14b2ee14545611cbda15c3736d9d68
Natan Nascimento <111619228+nataannn@users.noreply.github.com>
2024-03-21 21:16:46 -0300

    Add files via upload

---
+++ salario.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+float main ()
+{
+    float svelho, snovo, aumento;
+    printf("Informe o primeiro salário :");
+    scanf("%f", &svelho);
+    
+    snovo= svelho * 1.25;
+    printf("Seu novo salário com o novo aumento é: %f", snovo);
+    
+    
+    
+    
+    return 0;
+}
