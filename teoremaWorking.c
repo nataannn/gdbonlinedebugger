d8caf343795b08e460e18a9f4a93f4b798fdd04b
Natan Nascimento <111619228+nataannn@users.noreply.github.com>
2024-03-21 21:35:53 -0300

    Add files via upload

---
+++ teoremaWorking.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+float main ()
+{
+    float catA, catB, hip, hip1;
+    
+    printf("Informe o primeiro cateto: ");
+    scanf("%f", &catA);
+    
+    printf("Informe o segundo cateto: ");
+    scanf("%f", &catB);
+    
+    hip= (catA * catA) + (catB * catB);
+    hip1=
+    printf("A hipotenusa destes dois catetos é: %f", hip1);
+
+    return 0;
+}
